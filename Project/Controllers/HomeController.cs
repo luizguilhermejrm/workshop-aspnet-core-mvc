9f0ff9c00447ea55abb4816004f7eaba1cf6851d
Luiz <luizguilhermejrm@hotmail.com>
2022-03-07 13:00:33 -0300

    First controller and Razor pages tests

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace Project.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Aluno"] = "Guilherme";
             return View();
         }
 
