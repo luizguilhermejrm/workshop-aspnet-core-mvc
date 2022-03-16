a74b844c6b97e1d58b9c802c7ec8ccccfc07d53c
luizguilhermejrm <luizguilhermejrm@hotmail.com>
2022-03-16 13:47:27 -0300

    SellerService and basic find all

---
+++ Project/Services/SellerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Models;
+
+namespace Project.Services
+{
+    public class SellerService
+    {
+        private readonly ProjectContext _context;
+
+        public SellerService(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- Project/Startup.cs
+++ Project/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Project.Models;
 using Project.Data;
+using Project.Services;
 
 namespace Project
 {
@@ -42,6 +43,7 @@ namespace Project
                     builder.MigrationsAssembly("Project")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
